9a08454ab7c746dcbdeeccd9cfbadc853cf6638a
PokeRen <103824765+RenSnek@users.noreply.github.com>
2023-10-04 11:56:26 +0100

    Added files from replit

---
+++ script.js
@@ -0,0 +1,88 @@
+var firstPlaythrough = false;
+var tutorialCompleted = true;
+var lastPlayed;
+var inventory = [];
+
+function loadCookieData() {
+	if (Cookies.get("lastPlayed")) {
+		lastPlayed = Cookies.get("lastPlayed");
+		firstPlaythrough = false;
+		tutorialCompleted = (Cookies.get("tutorialCompleted") === 'true');
+		inventory = Cookies.get("inventory");
+	} else {
+		firstPlaythrough = true;
+		tutorialCompleted = false;
+		lastPlayed = Date.now();
+		inventory = [];
+	}
+}
+
+function saveCookieData() {
+	Cookies.set("lastPlayed",Date.now());
+	Cookies.set("inventory",inventory);
+	Cookies.set("tutorialCompleted",tutorialCompleted);
+}
+
+loadCookieData();
+
+window.onbeforeunload = function(){
+   //saveCookieData();
+}
+
+function updateInventory() {
+	document.getElementById("inventory").innerHTML = "";
+	var countedInventory = {}
+	for (var item of inventory) {
+		if (countedInventory[item]) {
+			countedInventory[item] = countedInventory[item]+1;
+		} else {
+			countedInventory[item] = 1;
+		}
+	}
+	//ADD HTML LIST
+	for (var [item, amt] of Object.entries(countedInventory)) {
+		var text = `${amt}x <span class="invItemName">${items[item].name}</span>`;
+		var elem = document.createElement("li");
+		elem.innerHTML = text;
+		elem.classList.add("inventoryItem");
+		document.getElementById("inventory").appendChild(elem);
+	}
+}
+
+inventory = ["test"];
+
+function selectTab(tab,tabs,tabPages) {
+	if (!tab.classList.contains("locked")) {
+		for (var loopTab of tabs) {
+			if (loopTab.dataset.tabgroup == tab.dataset.tabgroup) {
+				loopTab.classList.remove("selected");
+			}
+		}
+		tab.classList.add("selected");
+		for (var page of tabPages) {
+			if (page.dataset.tab == tab.dataset.tab) {
+				page.classList.remove("hide");
+			} else {
+				page.classList.add("hide");
+			}
+		}
+	}
+}
+
+function updateTabs() {
+	var tabs = document.getElementsByClassName("tab");
+	var tabPages = document.getElementsByClassName("tabContents");
+	for (var tab of tabs) {
+		tab.onclick = function(){ selectTab(this,tabs,tabPages); }
+		if (tab.classList.contains("selected")) {
+			tab.click();
+		}
+	}
+}
+
+updateTabs();
+
+if (!tutorialCompleted) {
+	document.getElementById("menu").classList.add("hide");
+	document.getElementById("tutorial").classList.remove("hide");
+}
